112f238e055a8e21836b58803def5caa805ce42f
Matt Johnson <matt9j@cs.washington.edu>
2020-09-15 17:50:15 -0700

    rccn: Rename get_local_extension for clarity
    In the split components the extension is consistently referred to as
    the msisdn. This commit updates the subscriber function call to
    indicate it returns an MSISDN from the provided IMSI.

--- rccn/modules/subscriber.py
+++ rccn/modules/subscriber.py
@@ -129,7 +129,7 @@ class Subscriber:
         except psycopg2.DatabaseError as e:
             raise SubscriberException('Database error in checking subscriber authorization: %s' % e)
 
-    def get_local_extension(self, imsi):
+    def get_msisdn_from_imsi(self, imsi):
         try:
             return self._osmo_hlr.get_msisdn_from_imsi(imsi)
         except OsmoHlrError as e:
@@ -560,7 +560,7 @@ class Subscriber:
     def add(self, msisdn, name, balance, location='', equipment=''):
         if len(msisdn) == 15:
             # lookup extension by imsi
-            extension = self.get_local_extension(msisdn)
+            extension = self.get_msisdn_from_imsi(msisdn)
             if len(extension) == 11:
                 extension=extension[-5:]
             imsi = msisdn
